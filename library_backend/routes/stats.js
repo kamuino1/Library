8007a3c4faf812b548dba75747b442bc2993a96f
PA Tiến <tienp323@gmail.com>
2024-11-26 22:33:25 +0700

    Add Stats

---
+++ library_backend/routes/stats.js
@@ -0,0 +1,86 @@
+import express from "express";
+import BookTransaction from "../models/BookTransaction.js";
+import User from "../models/User.js";
+import Book from "../models/Book.js";
+
+const router = express.Router();
+
+router.get("/most-borrowed-books", async (req, res) => {
+  try {
+    const mostBorrowedBooks = await BookTransaction.aggregate([
+      {
+        $match: { transactionType: "Issued" },
+      },
+      {
+        $group: {
+          _id: "$bookId",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { count: -1 },
+      },
+      {
+        $limit: 10,
+      },
+    ]);
+
+    const booksWithDetails = await Promise.all(
+      mostBorrowedBooks.map(async (book) => {
+        const bookDetails = await Book.findById(book._id);
+        return {
+          bookId: book._id,
+          bookName: bookDetails ? bookDetails.bookName : "Unknown",
+          borrowCount: book.count,
+        };
+      })
+    );
+
+    res.status(200).json(booksWithDetails);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "Lỗi khi thống kê các đầu sách được mượn nhiều nhất" });
+  }
+});
+
+router.get("/most-active-users", async (req, res) => {
+  try {
+    const mostActiveUsers = await BookTransaction.aggregate([
+      {
+        $match: { transactionType: "Issued" },
+      },
+      {
+        $group: {
+          _id: "$userId",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { count: -1 },
+      },
+      {
+        $limit: 10,
+      },
+    ]);
+
+    const usersWithDetails = await Promise.all(
+      mostActiveUsers.map(async (user) => {
+        const userDetails = await User.findById(user._id);
+        return {
+          userId: user._id,
+          userName: userDetails ? userDetails.fullName : "Unknown",
+          borrowCount: user.count,
+        };
+      })
+    );
+
+    res.status(200).json(usersWithDetails);
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "Lỗi khi thống kê danh sách người mượn nhiều sách nhất" });
+  }
+});
--- library_frontend/src/Pages/Dashboard/AdminDashboard/Components/AddBook.js
+++ library_frontend/src/Pages/Dashboard/AdminDashboard/Components/AddBook.js
@@ -1,152 +1,232 @@
-import React, { useContext, useEffect, useState } from 'react'
-import "../AdminDashboard.css"
-import axios from "axios"
-import { AuthContext } from '../../../../Context/AuthContext'
-import { Dropdown } from 'semantic-ui-react'
+import React, { useContext, useEffect, useState } from "react";
+import "../AdminDashboard.css";
+import axios from "axios";
+import { AuthContext } from "../../../../Context/AuthContext";
+import { Dropdown } from "react-bootstrap";
 
 function AddBook() {
+  const API_URL = process.env.REACT_APP_API_URL;
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useContext(AuthContext);
 
-    const API_URL = process.env.REACT_APP_API_URL
-    const [isLoading, setIsLoading] = useState(false)
-    const { user } = useContext(AuthContext)
+  const [bookName, setBookName] = useState("");
+  const [alternateTitle, setAlternateTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [bookCountAvailable, setBookCountAvailable] = useState(null);
+  const [language, setLanguage] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [allCategories, setAllCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [recentAddedBooks, setRecentAddedBooks] = useState([]);
 
-    const [bookName, setBookName] = useState("")
-    const [alternateTitle, setAlternateTitle] = useState("")
-    const [author, setAuthor] = useState("")
-    const [bookCountAvailable, setBookCountAvailable] = useState(null)
-    const [language, setLanguage] = useState("")
-    const [publisher, setPublisher] = useState("")
-    const [allCategories, setAllCategories] = useState([])
-    const [selectedCategories, setSelectedCategories] = useState([])
-    const [recentAddedBooks, setRecentAddedBooks] = useState([])
+  /* Fetch all the Categories */
+  useEffect(() => {
+    const getAllCategories = async () => {
+      try {
+        const response = await axios.get(
+          API_URL + "api/categories/allcategories"
+        );
+        const all_categories = await response.data.map((category) => ({
+          value: `${category._id}`,
+          label: `${category.categoryName}`,
+        }));
+        setAllCategories(all_categories);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAllCategories();
+  }, [API_URL]);
 
-
-    /* Fetch all the Categories */
-    useEffect(() => {
-        const getAllCategories = async () => {
-            try {
-                const response = await axios.get(API_URL + "api/categories/allcategories")
-                const all_categories = await response.data.map(category => (
-                    { value: `${category._id}`, text: `${category.categoryName}` }
-                ))
-                setAllCategories(all_categories)
-            }
-            catch (err) {
-                console.log(err)
-            }
-        }
-        getAllCategories()
-    }, [API_URL])
-
-    /* Adding book function */
-    const addBook = async (e) => {
-        e.preventDefault()
-        setIsLoading(true)
-        const BookData = {
-            bookName: bookName,
-            alternateTitle: alternateTitle,
-            author: author,
-            bookCountAvailable: bookCountAvailable,
-            language: language,
-            publisher: publisher,
-            categories: selectedCategories,
-            isAdmin: user.isAdmin
-        }
-        try {
-            const response = await axios.post(API_URL + "api/books/addbook", BookData)
-            if (recentAddedBooks.length >= 5) {
-                recentAddedBooks.splice(-1)
-            }
-            setRecentAddedBooks([response.data, ...recentAddedBooks])
-            setBookName("")
-            setAlternateTitle("")
-            setAuthor("")
-            setBookCountAvailable(null)
-            setLanguage("")
-            setPublisher("")
-            setSelectedCategories([])
-            alert("Book Added Successfully 🎉")
-        }
-        catch (err) {
-            console.log(err)
-        }
-        setIsLoading(false)
+  /* Adding book function */
+  const addBook = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const BookData = {
+      bookName: bookName,
+      alternateTitle: alternateTitle,
+      author: author,
+      bookCountAvailable: bookCountAvailable,
+      language: language,
+      publisher: publisher,
+      categories: selectedCategories,
+      isAdmin: user.isAdmin,
+    };
+    try {
+      const response = await axios.post(
+        API_URL + "api/books/addbook",
+        BookData
+      );
+      if (recentAddedBooks.length >= 5) {
+        recentAddedBooks.splice(-1);
+      }
+      setRecentAddedBooks([response.data, ...recentAddedBooks]);
+      setBookName("");
+      setAlternateTitle("");
+      setAuthor("");
+      setBookCountAvailable(null);
+      setLanguage("");
+      setPublisher("");
+      setSelectedCategories([]);
+      alert("Book Added Successfully 🎉");
+    } catch (err) {
+      console.log(err);
     }
+    setIsLoading(false);
+  };
 
+  useEffect(() => {
+    const getallBooks = async () => {
+      const response = await axios.get(API_URL + "api/books/allbooks");
+      setRecentAddedBooks(response.data.slice(0, 5));
+    };
+    getallBooks();
+  }, [API_URL]);
 
-    useEffect(() => {
-        const getallBooks = async () => {
-            const response = await axios.get(API_URL + "api/books/allbooks")
-            setRecentAddedBooks(response.data.slice(0, 5))
-        }
-        getallBooks()
-    }, [API_URL])
-
-
-    return (
-        <div>
-            <p className="dashboard-option-title">Add a Book</p>
-            <div className="dashboard-title-line"></div>
-            <form className='addbook-form' onSubmit={addBook}>
-
-                <label className="addbook-form-label" htmlFor="bookName">Book Name<span className="required-field">*</span></label><br />
-                <input className="addbook-form-input" type="text" name="bookName" value={bookName} onChange={(e) => { setBookName(e.target.value) }} required></input><br />
-
-                <label className="addbook-form-label" htmlFor="alternateTitle">AlternateTitle</label><br />
-                <input className="addbook-form-input" type="text" name="alternateTitle" value={alternateTitle} onChange={(e) => { setAlternateTitle(e.target.value) }}></input><br />
-
-                <label className="addbook-form-label" htmlFor="author">Author Name<span className="required-field">*</span></label><br />
-                <input className="addbook-form-input" type="text" name="author" value={author} onChange={(e) => { setAuthor(e.target.value) }} required></input><br />
-
-                <label className="addbook-form-label" htmlFor="language">Language</label><br />
-                <input className="addbook-form-input" type="text" name="language" value={language} onChange={(e) => { setLanguage(e.target.value) }}></input><br />
-
-                <label className="addbook-form-label" htmlFor="publisher">Publisher</label><br />
-                <input className="addbook-form-input" type="text" name="publisher" value={publisher} onChange={(e) => { setPublisher(e.target.value) }}></input><br />
-
-                <label className="addbook-form-label" htmlFor="copies">No.of Copies Available<span className="required-field">*</span></label><br />
-                <input className="addbook-form-input" type="text" name="copies" value={bookCountAvailable} onChange={(e) => { setBookCountAvailable(e.target.value) }} required></input><br />
-
-                <label className="addbook-form-label" htmlFor="categories">Categories<span className="required-field">*</span></label><br />
-                <div className="semanticdropdown">
-                    <Dropdown
-                        placeholder='Category'
-                        fluid
-                        multiple
-                        search
-                        selection
-                        options={allCategories}
-                        value={selectedCategories}
-                        onChange={(event, value) => setSelectedCategories(value.value)}
-                    />
-                </div>
-
-                <input className="addbook-submit" type="submit" value="SUBMIT" disabled={isLoading}></input>
-            </form>
-            <div>
-                <p className="dashboard-option-title">Recently Added Books</p>
-                <div className="dashboard-title-line"></div>
-                <table className='admindashboard-table'>
-                    <tr>
-                        <th>S.No</th>
-                        <th>Book Name</th>
-                        <th>Added Date</th>
-                    </tr>
-                    {
-                        recentAddedBooks.map((book, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>{book.bookName}</td>
-                                    <td>{book.createdAt.substring(0, 10)}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </table>
-            </div>
+  return (
+    <div>
+      <p className="dashboard-option-title">Add a Book</p>
+      <div className="dashboard-title-line"></div>
+      <form className="addbook-form" onSubmit={addBook}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="bookName">
+            Book Name<span className="required-field">*</span>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            name="bookName"
+            value={bookName}
+            onChange={(e) => {
+              setBookName(e.target.value);
+            }}
+            required
+          />
         </div>
-    )
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="alternateTitle">
+            Alternate Title
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            name="alternateTitle"
+            value={alternateTitle}
+            onChange={(e) => {
+              setAlternateTitle(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="author">
+            Author Name<span className="required-field">*</span>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            name="author"
+            value={author}
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="language">
+            Language
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            name="language"
+            value={language}
+            onChange={(e) => {
+              setLanguage(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="publisher">
+            Publisher
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            name="publisher"
+            value={publisher}
+            onChange={(e) => {
+              setPublisher(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="copies">
+            No. of Copies Available<span className="required-field">*</span>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            name="copies"
+            value={bookCountAvailable}
+            onChange={(e) => {
+              setBookCountAvailable(e.target.value);
+            }}
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="categories">
+            Categories<span className="required-field">*</span>
+          </label>
+          <br />
+          <Dropdown
+            placeholder="Select Category"
+            fluid
+            multiple
+            search
+            selection
+            options={allCategories}
+            value={selectedCategories}
+            onChange={(event, value) => setSelectedCategories(value.value)}
+          />
+        </div>
+
+        <button className="btn btn-primary" type="submit" disabled={isLoading}>
+          Submit
+        </button>
+      </form>
+
+      <div>
+        <p className="dashboard-option-title">Recently Added Books</p>
+        <div className="dashboard-title-line"></div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Book Name</th>
+              <th>Added Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {recentAddedBooks.map((book, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{book.bookName}</td>
+                <td>{book.createdAt.substring(0, 10)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default AddBook
+export default AddBook;
